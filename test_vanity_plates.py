de566f78fc674989edae60541fd455b99036d360
alexgicimo <gakurualexgicimo@gmail.com>
2024-11-25 09:53:38 +0000

    testing whether the function checking if a plate begins with two letters works as aimed for

--- test_vanity_plates.py
+++ test_vanity_plates.py
@@ -10,3 +10,15 @@ class TestIsAlphaNum:
     plates = Plates()
     def test_is_alpha_num(self,plate, result)->None:
         assert self.plates.is_alpha_num(plate) == result
+
+@pytest.mark.parametrize("plate, result", [
+    ("Hello", True),
+    ("123Hel", False),
+    ("1234", False),
+    ("Hello23", True)
+])
+class TestBeginsWithTwoLetters:
+    plates = Plates()
+    
+    def test_begins_with_two_letters(self,plate, result)->None:
+        assert self.plates.begins_with_two_letters(plate) == result
