79ae7c746c81105a54df485029207d09af44d41c
alexgicimo <gakurualexgicimo@gmail.com>
2024-11-25 10:00:14 +0000

    testing if the function checking for a valid length plate works fine

--- test_vanity_plates.py
+++ test_vanity_plates.py
@@ -1,5 +1,7 @@
 from vanity_plates import Plates
 import pytest
+
+# Test for the function checking for both digits and letters in plate
 @pytest.mark.parametrize("plate, result", [
     ("Hello1234", True),
     ("1234", False),
@@ -11,6 +13,7 @@ class TestIsAlphaNum:
     def test_is_alpha_num(self,plate, result)->None:
         assert self.plates.is_alpha_num(plate) == result
 
+# Test for the begins_with_two_letters function
 @pytest.mark.parametrize("plate, result", [
     ("Hello", True),
     ("123Hel", False),
@@ -19,6 +22,21 @@ class TestIsAlphaNum:
 ])
 class TestBeginsWithTwoLetters:
     plates = Plates()
-    
+
     def test_begins_with_two_letters(self,plate, result)->None:
         assert self.plates.begins_with_two_letters(plate) == result
+
+#Test for whether the length is valid
+@pytest.mark.parametrize("plate, result",[
+    ("Hello", True),
+    ("HELLO", True),
+    ("HELLOWORLD", False),
+    ("helloworld", False),
+    ("Hello1", True),
+    ("Goodbye", False)
+])
+class TestIsLengthValid:
+    plates = Plates()
+
+    def test_is_length_valid(self,plate, result)->None:
+        assert self.plates.is_length_valid(plate) == result 
